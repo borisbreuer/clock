291d55259c81b580af5149d2f8bbdfa6f23ed666
Boris Breuer <boris.breuer@respondi.com>
2019-10-02 18:15:08 +0200

    objects 'n Orbit

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,6 @@
+
+
+
 /*
 1° × π / 180
 360 / 12 = 30
@@ -7,97 +10,131 @@ x - Koordinate = cos(i * winkel) * radius
 y - Koordinate = sin(i * winkel) * radius
   */
 
+
 (function () {
-  const elements = 12;
+  "use strict"
+
   const root = document.querySelector('#clock')
+  const clockX = root.offsetWidth / 2;
+  const clockY = root.offsetHeight / 2;
 
-  let x = root.offsetWidth / 2;
-  let y = root.offsetHeight / 2;
-
-  for (let i = 0; i < elements; i++) {
-    let ranAngle = 30 * Math.PI / 180;
-    let pointX = Math.cos((i - 3) * ranAngle) * 225;
-    let pointY = Math.sin((i - 3) * ranAngle) * 225;
-
-    let point = document.createElement('div');
-    point.className = 'point';
-    point.style.left = x + pointX + 'px';
-    point.style.top = y + pointY + 'px';
-    root.appendChild(point);
-
-    let pointX2 = Math.cos((i - 3) * ranAngle) * 250;
-    let pointY2 = Math.sin((i - 3) * ranAngle) * 250;
-
-    let count = document.createElement('div');
-    count.className = 'count';
-    count.style.left = x + pointX2 + 'px';
-    count.style.top = y + pointY2 + 'px';
-
-    count.textContent = i == 0 ? 12 : i;
-    root.appendChild(count);
+  const CircleCoords = function (degAngle, radius, index) {
+    this.radAngle = degAngle * Math.PI / 180;
+    this.X = Math.cos(index * this.radAngle) * radius;
+    this.Y = Math.sin(index * this.radAngle) * radius;
+    return { X: this.X, Y: this.Y }
   }
 
-  let ms = document.createElement('div');
-  ms.className = 'msPointer';
-  ms.style.left = x + 'px';
-  ms.style.top = y + 'px';
+  const ClockPointer = function (element, className, x, y) {
+    this.obj = document.createElement(element);
+    this.obj.className = className;
+    this.obj.style.left = x + 'px';
+    this.obj.style.top = y + 'px';
+    return this.obj;
+  }
 
-  let s = document.createElement('div');
-  s.className = 'sPointer';
-  s.style.left = x + 'px';
-  s.style.top = y + 'px';
+  let
+    hours,
+    hoursText,
+    minutes,
+    hoursCoords,
+    hoursTextCoords,
+    minutesCoords,
+    time, msec, sec, min, hou,
+    msDeg, sDeg, mDeg, hDeg,
+    continousOrNot = false,
+    count = 0;
 
-  let h = document.createElement('div');
-  h.className = 'hPointer';
-  h.style.left = x + 'px';
-  h.style.top = y + 'px';
+  root.addEventListener('mouseenter', function () {
+    continousOrNot = true;
+  })
+  root.addEventListener('mouseleave', function () {
+    continousOrNot = false;
+  })
 
-  let m = document.createElement('div');
-  m.className = 'mPointer';
-  m.style.left = x + 'px';
-  m.style.top = y + 'px';
 
-  let mid = document.createElement('div');
-  mid.className = 'middle';
-  mid.style.left = x + 'px';
-  mid.style.top = y + 'px';
+  for (let i = 0; i < 60; i++) {
+    if (!(i % 5)) {
+      hoursCoords = new CircleCoords(360 / 12, 250, count - 3);
+      hoursTextCoords = new CircleCoords(360 / 12, 225, count - 3);
 
-  root.appendChild(ms);
+      hours = document.createElement('div');
+      hours.className = count % 3 ? 'hoursPoint' : 'hoursPointBig';
+      hours.style.left = clockX + hoursCoords.X + 'px';
+      hours.style.top = clockY + hoursCoords.Y + 'px';
+      root.appendChild(hours);
+
+      hoursText = document.createElement('div');
+      hoursText.className = 'hoursText';
+      hoursText.style.left = clockX + hoursTextCoords.X + 'px';
+      hoursText.style.top = clockY + hoursTextCoords.Y + 'px';
+      hoursText.textContent = count == 0 ? 12 : count;
+      root.appendChild(hoursText);
+
+      count++;
+    }
+
+    if (i % 5) {
+      minutesCoords = new CircleCoords(360 / 60, 250, i);
+      minutes = document.createElement('div');
+      minutes.className = 'minutes';
+      minutes.style.left = clockX + minutesCoords.X + 'px';
+      minutes.style.top = clockY + minutesCoords.Y + 'px';
+      root.appendChild(minutes);
+    }
+  }
+
+  let ms = new ClockPointer('div', 'msPointer', clockX, clockY)
+  let s = new ClockPointer('div', 'sPointer', clockX, clockY)
+  let m = new ClockPointer('div', 'mPointer', clockX, clockY)
+  let h = new ClockPointer('div', 'hPointer', clockX, clockY)
+  let mid = new ClockPointer('div', 'middle', clockX, clockY)
+  let msdot = new ClockPointer('div', 'msDot', clockX, clockY)
+
+  root.appendChild(ms)
   root.appendChild(s);
   root.appendChild(h);
   root.appendChild(m);
   root.appendChild(mid);
+  root.appendChild(msdot);
 
-  let time, msec, sec, min, hou, now, sDeg, mDeg, hDeg;
-  let msecSec = 0;
+
+
   function timer() {
     time = new Date();
     msec = time.getMilliseconds();
     sec = time.getSeconds();
     min = time.getMinutes();
     hou = time.getHours();
-    now = Date.now();
-    // sec = 20;
-    // min = 15;
-    // hou = 9;
 
+    // msec = 0;
+    // sec = 30;
+    // min = 59;
+    // hou = 5;
 
-
-    msDeg = (msec / 1000) * 360;
-    // sDeg = sec * 360 / 60;
-    sDeg = ((msec / 1000) + sec) / 60 * 360;
-    mDeg = ((sec / 60) + (min + (msec / 1000) / 60)) / 60 * 360;
-    hDeg = ((min / 60) + (hou + (min / 60) / 12)) / 12 * 360;
+    if (continousOrNot) {
+      msDeg = (msec / 1000) * 360;
+      sDeg = ((msec / 1000) + sec) / 60 * 360;
+      mDeg = ((sec / 60) + (min + (msec / 1000) / 60)) / 60 * 360;
+      hDeg = ((min / 60) + (hou + (min / 60) / 60)) / 12 * 360;
+    } else {
+      msDeg = msec / 1000 * 360;
+      sDeg = sec / 60 * 360;
+      mDeg = min / 60 * 360;
+      hDeg = hou / 12 * 360;
+    }
 
     ms.style.transform = 'translate(-50%, -50%) rotate(' + (msDeg - 90) + 'deg)';
     s.style.transform = 'translate(-50%, -50%) rotate(' + (sDeg - 90) + 'deg)';
     h.style.transform = 'translate(-50%, -50%) rotate(' + (hDeg - 90) + 'deg)';
     m.style.transform = 'translate(-50%, -50%) rotate(' + (mDeg - 90) + 'deg)';
 
-    // document.querySelector('#timer').value = hou + ':' + min + ':' + sec
-    // document.querySelector('#timer').value = ((hou / 60) / 60) / 1000
+    let sdotCoords = new CircleCoords(-sDeg + 180, 143.75, 1);
+
+    ms.style.top = clockX + sdotCoords.X + 'px';
+    ms.style.left = clockY + sdotCoords.Y + 'px';
+
     window.requestAnimationFrame(timer);
   }
   timer();
-  // setInterval(timer, 1)
 })();
